107d723988406c59c69604622e8f509680a05049
AS-2K20 <amudhan.psgcas@gmail.com>
2020-07-14 06:24:29 +0530

    JDBC Connectivity Lab

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,20 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
-		
-	// Fill your code
-		
+	public static void main(String args[]) throws ClassNotFoundException, SQLException {
+	Connection con = null;
+	ConnectionManager cm = new ConnectionManager();
+	con=cm.getConnection();	
+	if(con!=null) {
+		System.out.println("Connection established");
+	}
+	else {
+		System.out.println("Check your connection");
+	}
 	}
 }
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,9 +1,16 @@
 package utility;
 
-
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionManager {
-	
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.OracleDriver");
+		Connection con = null;
+		con = DriverManager.getConnection("jdbc:oracle:thin:system/Amudhan.123@localhost:1521:orcle");
+
+		return con;		
+	}
 	
 }
